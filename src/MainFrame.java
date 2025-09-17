ed3f5aaced0d65d510639629502254472117179a
JaviruloNT <albapeque2015@gmail.com>
2025-09-17 16:20:53 -0300

    Creado Main Principal

---
+++ src/MainFrame.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    JLabel title;
+    JButton registerButton;
+    JButton loginButton;
+    final int WIDTH = 300;
+    final int HEIGHT = 400;
+    public MainFrame() {
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+        this.setResizable(false);
+        this.setSize(WIDTH,HEIGHT);
+        // Label Bienvenida
+        title = new JLabel("Sistema");
+        title.setPreferredSize(new Dimension(150,50));
+        title.setFocusable(false);
+        title.setHorizontalAlignment(0);
+        title.setVerticalAlignment(0);
+        this.add(title);
+        // Boton Registro
+        registerButton = new JButton("Registrar");
+        registerButton.addActionListener(e -> new RegisterFrame());
+        registerButton.setFocusable(false);
+        registerButton.setPreferredSize(new Dimension(150,50));
+        this.add(registerButton);
+        // Boton Login
+        loginButton = new JButton("Ingresar");
+        loginButton.addActionListener(e -> System.out.println("Login!"));
+        loginButton.setFocusable(false);
+        loginButton.setPreferredSize(new Dimension(150,50));
+        this.add(loginButton);
+        //
+        this.setVisible(true);
+    }
+    //
+
+    public static void main(String[] args) {
+        new MainFrame();
+    }
+}
