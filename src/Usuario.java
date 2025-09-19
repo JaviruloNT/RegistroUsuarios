7ed5500f7e86e82b1d587b7a24e5da1e54dadd4e
JaviruloNT <albapeque2015@gmail.com>
2025-09-19 02:11:59 -0300

    Quitado codigo de usuarios

--- src/Usuario.java
+++ src/Usuario.java
@@ -1,25 +1,13 @@
 public class Usuario {
-    private static int cantUsuario;
-    private int codigo;
     private String usuario;
     private char[] contrasena;
     //
     public Usuario() {}
     public Usuario(String usuario, char[] contrasena) {
-        cantUsuario++;
-        this.codigo = cantUsuario;
         this.usuario = usuario;
         this.contrasena = contrasena;
     }
     //
-    public int getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(int codigo) {
-        this.codigo = codigo;
-    }
-
     public void setUsuario(String usuario) {
         this.usuario = usuario;
     }
