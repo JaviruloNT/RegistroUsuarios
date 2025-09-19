64cee73081baefe863109945a13d970f06d59bab
JaviruloNT <albapeque2015@gmail.com>
2025-09-18 21:16:27 -0300

    Creado frame de inicio de sesion

---
+++ src/LoginFrame.java
@@ -0,0 +1,113 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.XMLDecoder;
+import java.io.*;
+import java.util.Arrays;
+
+public class LoginFrame extends JFrame implements ActionListener {
+    boolean buttonsEnabled = true;
+    JLabel title;
+    JLabel usrLabel;
+    JTextField usrField;
+    JLabel pwLabel;
+    JPasswordField pwField;
+    JButton registerButton;
+    final int WIDTH = 300;
+    final int HEIGHT = 400;
+    //
+    public LoginFrame() {
+        this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        this.setLayout(new FlowLayout());
+        this.setResizable(false);
+        this.setSize(WIDTH,HEIGHT);
+        this.setLocationRelativeTo(null);
+        // Label Titulo
+        title = new JLabel("Inicie sesion");
+        title.setPreferredSize(new Dimension(200,50));
+        title.setFocusable(false);
+        title.setHorizontalAlignment(0);
+        title.setVerticalAlignment(0);
+        this.add(title);
+        // Label Usuario
+        usrLabel = new JLabel("Usuario:");
+        usrLabel.setPreferredSize(new Dimension(100,50));
+        usrLabel.setFocusable(false);
+        usrLabel.setHorizontalAlignment(0);
+        usrLabel.setVerticalAlignment(0);
+        this.add(usrLabel);
+        // Field Usuario
+        usrField = new JTextField();
+        usrField.setPreferredSize(new Dimension(100,25));
+        usrField.addActionListener(this);
+        this.add(usrField);
+        // Label Contrasena
+        pwLabel = new JLabel("Contrasena:");
+        pwLabel.setPreferredSize(new Dimension(100,50));
+        pwLabel.setFocusable(false);
+        pwLabel.setHorizontalAlignment(0);
+        pwLabel.setVerticalAlignment(0);
+        this.add(pwLabel);
+        // Field Contrasena
+        pwField = new JPasswordField();
+        pwField.setPreferredSize(new Dimension(100,25));
+        pwField.addActionListener(this);
+        this.add(pwField);
+        // Button Registrar
+        registerButton = new JButton("Registrar");
+        registerButton.setPreferredSize(new Dimension(100,50));
+        registerButton.addActionListener(this);
+        registerButton.setFocusable(false);
+        this.add(registerButton);
+        //
+        this.setVisible(true);
+    }
+    //
+
+    public static void main(String[] args) {
+        new LoginFrame();
+    }
+
+    // Activar/desactivar botones
+    private void toggleButtons() {
+        buttonsEnabled = !buttonsEnabled;
+        registerButton.setEnabled(buttonsEnabled);
+        usrField.setEnabled(buttonsEnabled);
+        pwField.setEnabled(buttonsEnabled);
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        toggleButtons();
+        // Verificar campos vacios
+        if (usrField.getText().isEmpty() || pwField.getPassword().length == 0) {
+            System.out.println("Usuario o contrasena vacios.");
+            toggleButtons();
+            return;
+        }
+        String path = "users/" + usrField.getText().toLowerCase() + ".xml";
+        File file = new File(path);
+        // Verificar si no existe usuario con ese nombre
+        if (!file.exists()) {
+            System.out.println("Usuario o contrasena incorrectos.");
+            toggleButtons();
+            return;
+        }
+        //  Iniciar sesion
+        Usuario usuario = null;
+        try (XMLDecoder decoder = new XMLDecoder(new FileInputStream(file))) {
+            usuario = (Usuario) decoder.readObject();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        // Verificar si la contrasena es correcta
+        if (!Arrays.equals(usuario.getContrasena(),pwField.getPassword())) {
+            System.out.println("Usuario o contrasena incorrectos.");
+            toggleButtons();
+            return;
+        }
+        // Cerrar
+        System.out.println("Acceso autorizado.");
+        this.dispose();
+    }
+}
